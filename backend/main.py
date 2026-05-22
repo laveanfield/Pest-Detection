e77da5b8460609483214d230a41808d6c0ca9dd6
laveanfield <hienchenkawaii@gmail.com>
2026-05-22 12:46:31 +0700

    add metric using grafana for visualizing and edit training.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.routes import predict, history, stats
 from db.database import engine, Base
+from prometheus_fastapi_instrumentator import Instrumentator
 from models.detection import Detection
 from models.model_version import ModelVersion
 from models.batch_summary import BatchSummary
@@ -10,6 +11,8 @@ from api.routes import models
 load_dotenv()
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 Base.metadata.create_all(bind=engine)
 
